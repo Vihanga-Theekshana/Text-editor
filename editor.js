438f6108aad83ba58ab71d96e5352b929c1677bd
Vihanga Theekshana <theekshanavihanga2@gmail.com>
2025-05-31 00:01:06 +0530

    fimal code

--- editor.js
+++ editor.js
@@ -1,12 +1,12 @@
 function edit(value){
-    document.execCommand(value,false,null);
+    document.execCommand(value,false,null);//edit text
   }
 function triggercolor(){
-  document.getElementById("fontcolor").click();
+  document.getElementById("colorselect").click();
 }
 function changefontcolor(){
-  let color = document.getElementById("fontcolor").value;
+  let color = document.getElementById("colorselect").value;//Assume to select color 
   document.execCommand("foreColor",false,color);
-  document.getElementById("underline").style.backgroundColor=color;
+  document.getElementById("underline").style.backgroundColor=color; //Add color to underline div
 }
 
